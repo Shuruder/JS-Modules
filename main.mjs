966735493c84b504f44acdc729c1e9de2837b6c3
Gabriel Brandini Fonseca <gabriel_brandini@hotmail.com>
2022-07-24 20:49:09 -0300

    JS-Modules concluido

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Gabriel', 27));
+console.log(mostraCidade('Gabriel', 'SP'));
+console.log(mostraHobby('Gabriel', 'Assistir Séries'));
